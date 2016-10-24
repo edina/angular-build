cc9603bd45a0f290cbf28b5e5d6eb63170b1c694
Andrew Seales <andrew.seales@ed.ac.uk>
2016-10-24 15:17:19 +0100

    Make sure the scripts-ngc task is synchronous to avoid race conditions.
    This task must complete before the scripts-typescript task otherwise the
    ngfactory files don't get compiled.

--- src/gulp/tasks/scripts-ngc.js
+++ src/gulp/tasks/scripts-ngc.js
@@ -7,9 +7,10 @@ class ScriptsTypeScriptTaskLoader extends AbstractTaskLoader {
     registerTask(gulp){
         super.registerTask(gulp);
 
-        gulp.task("scripts-ngc", "Run the Angular 2 NGC compiler", () =>{
+        gulp.task("scripts-ngc", "Run the Angular 2 NGC compiler", (callback) =>{
             exec("./node_modules/.bin/ngc -p tsconfig-aot.json", (err, stdout, stderr) =>{
                 console.log(stderr);
+                callback();
             });
         });
     }
